085c8483e1224ab64a6178ce4ba607ca4d29f705
Kacper <kacpermurawski2000@gmail.com>
2024-04-15 17:24:48 +0200

    15.04.2024

---
+++ AttachmentsDownloader/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+namespace AttachmentsDownloader;
+
+class Program
+{
+    
+    static void Main(string[] args)
+    {
+        var userId = "kacpermurawski2000@gmail.com";
+        
+        var initializer = new BaseClientService.Initializer();
+        // set properties on intializer
+        using var gmail = new GmailService(initializer);
+
+        var messageIds = gmail
+            .Users
+            .Messages
+            .List(userId)
+            .Execute()
+            .Select(message => message.Id);
+
+        foreach(var messageId in messageIds)
+        {
+            var message = gmail
+                .Users
+                .Message
+                .Get(userId, messageId)
+                .Execute();
+            var attachmentId = message
+                .Payload
+                .Body
+                .AttachmentId;
+            if(attachmentId is null)
+                continue;
+
+            var base64Data = gmail
+                .Users
+                .Messages
+                .Attachments
+                .Get(userId, messageId, attachmentId)
+                .Execute()
+                .Data;
+
+            // do something with attachment
+        }
+    }
+}
